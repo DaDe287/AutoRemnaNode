2b0c40070036f591c1ef6ad2def0bdd993da56f9
Anatoli <64985361+DaDe287@users.noreply.github.com>
2026-02-05 19:17:59 +0300

    Update setup-remnaNode.sh

--- setup-remnaNode.sh
+++ setup-remnaNode.sh
@@ -1,3 +1,9 @@
+# Block Any scanners to the server
+sudo traffic-guard full \
+  -u https://raw.githubusercontent.com/shadow-netlab/traffic-guard-lists/refs/heads/main/public/antiscanner.list \
+  -u https://raw.githubusercontent.com/shadow-netlab/traffic-guard-lists/refs/heads/main/public/government_networks.list \
+  --enable-logging
+
 curl -fsSL https://get.docker.com | sh
 
 mkdir /opt/remnanode && cd /opt/remnanode
